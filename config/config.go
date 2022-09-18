5d582d7547f06ba104210f7d6f99f55e49d6b9ac
ditsuke <ditsuke@protonmail.com>
2022-09-18 17:42:54 +0530

    feat: extend config
    Adds server host/port, video query and poll intervals
    to the environment-derived config.

--- config/config.go
+++ config/config.go
@@ -1,11 +1,16 @@
 package config
 
 type Config struct {
-	YouTubeAPIKey string `env:"YOUTUBE_API_KEY"`
+	YouTubeAPIKey       string `env:"YOUTUBE_API_KEY"`
+	YouTubeVideoQuery   string `env:"YOUTUBE_VIDEO_QUERY"`
+	YouTubePollInterval int    `env:"YOUTUBE_POLL_INTERVAL"`
 
 	PostgresHost string `env:"PGHOST,default=localhost"`
 	PostgresPort string `env:"PGPORT,default=5432"`
 	PostgresUser string `env:"PGUSER"`
 	PostgresPass string `env:"PGPASSWORD"`
 	PostgresDB   string `env:"PGDB"`
+
+	ServerPort string `env:"PORT,default=8080"`
+	ServerHost string `env:"HOST,default=localhost"`
 }
